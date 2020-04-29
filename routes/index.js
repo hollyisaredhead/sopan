0cde2264ee4cdb2f2c2b47567cfa3457600feb10
kyle074613 <kylemj@uci.edu>
2020-04-28 20:02:29 -0700

    fixing heroku?

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const path = require("path");
 
 module.exports = (app, io) => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname + '/build/index.html'));
-    });
 
     require("./api/apiRoutes.js")(app);
     require("./io/ioRoutes.js")(io);
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
 require("./routes")(app, io);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userlist");

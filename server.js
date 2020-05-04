337179cfce0b204893fa0b38bb0d0080a8b99378
kyle074613 <kylemj@uci.edu>
2020-05-03 17:27:37 -0700

    added dependencies and started routes with authentication

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const mongoose = require("mongoose");
+const cors = require('cors');
+const helmet = require('helmet');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 var PORT = process.env.PORT || 3001
 
 require("dotenv").config();
@@ -10,6 +14,9 @@ require("dotenv").config();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));

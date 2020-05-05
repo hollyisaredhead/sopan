6e70dd3c235378340dbf467972b87889960151a4
Michael Le <mnle529@gmail.com>
2020-05-04 23:18:49 -0700

    taking out unused components

--- client/src/components/SecuredRoute/SecuredRoute.js
+++ client/src/components/SecuredRoute/SecuredRoute.js
@@ -5,7 +5,7 @@ import auth0Client from '../../utils/Auth';
 function SecuredRoute(props) {
     const { component: Component, path } = props;
     return (
-        <Route path={props.path} render={() => {
+        <Route path={path} render={() => {
             if (props.checkingSession) return <h3>Validating session...</h3>;
             if (!auth0Client.isAuthenticated()) {
                 auth0Client.signIn();

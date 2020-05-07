55e41625a69f9c3df726d8e9e8523ae10b232710
kyle074613 <kylemj@uci.edu>
2020-05-06 22:40:51 -0700

    removed one console.log

--- client/src/components/ViewingRoomLayout/RoomGridLayout.js
+++ client/src/components/ViewingRoomLayout/RoomGridLayout.js
@@ -98,8 +98,6 @@ export default function RoomLayout() {
         socket.on('update users', function (currentUsers) {
             updateUsers(currentUsers);
         });
-
-        console.log(auth0Client.profile)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
